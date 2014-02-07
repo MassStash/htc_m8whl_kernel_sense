48044bb396e2d313385e020cb5a1fbbccf241e1a
Sheetal Sahasrabudhe <sheetals@codeaurora.org>
2014-02-07 13:47:00 -0500

    Perf: Reset pmu after hotplug
    The counter enable bits were staying in undefined state after CPU
    came out of hotplug when perf was not enabled.
    This was resulting into non-zero values for counters in ftrace buffer
    which were not enabled by perf.
    Clear the pmu when CPU comes out of hotplug and perf is not enabled.
    
    Change-Id: I31e17565ad914650a12102a5c359b363f8f2ca2a
    Signed-off-by: Sheetal Sahasrabudhe <sheetals@codeaurora.org>
    Signed-off-by: MassStash <MassStash@gmail.com>

--- arch/arm/kernel/perf_event.c
+++ arch/arm/kernel/perf_event.c
@@ -836,6 +836,24 @@ static int __cpuinit pmu_cpu_notify(struct notifier_block *b,
 					unsigned long action, void *hcpu)
 {
 	int irq;
+	struct pmu *pmu;
+	int cpu = (int)hcpu;
+
+	switch ((action & ~CPU_TASKS_FROZEN)) {
+	case CPU_DOWN_PREPARE:
+		if (cpu_pmu && cpu_pmu->save_pm_registers)
+			smp_call_function_single(cpu,
+						 cpu_pmu->save_pm_registers,
+						 hcpu, 1);
+		break;
+	case CPU_STARTING:
+		if (cpu_pmu && cpu_pmu->reset)
+			cpu_pmu->reset(NULL);
+		if (cpu_pmu && cpu_pmu->restore_pm_registers)
+			smp_call_function_single(cpu,
+						 cpu_pmu->restore_pm_registers,
+						 hcpu, 1);
+	}
 
 	if (cpu_has_active_perf((int)hcpu)) {
 		switch ((action & ~CPU_TASKS_FROZEN)) {
@@ -866,9 +884,10 @@ static int __cpuinit pmu_cpu_notify(struct notifier_block *b,
 			}
 			return NOTIFY_DONE;
 
-		case CPU_STARTING:
-			if (cpu_pmu && cpu_pmu->reset) {
-				cpu_pmu->reset(NULL);
+			if (cpu_pmu) {
+				__get_cpu_var(from_idle) = 1;
+				pmu = &cpu_pmu->pmu;
+				pmu->pmu_enable(pmu);
 				return NOTIFY_OK;
 			}
 		default:
