bf54ee51db7a21d5df5c64ff9af695497805d218
MassStash <MassStash@gmail.com>
2014-10-08 22:33:55 -0500

    cpu-boost: Fuk off a boost_ms off be default; thanks to Dan for the kill settings
    Signed-off-by: MassStash <MassStash@gmail.com>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -41,17 +41,17 @@ static DEFINE_PER_CPU(struct cpu_sync, sync_info);
 static DEFINE_PER_CPU(struct task_struct *, thread);
 static struct workqueue_struct *cpu_boost_wq;
 
-static unsigned int boost_ms;
-module_param(boost_ms, uint, 0644);
-
-static unsigned int sync_threshold;
+static unsigned int sync_threshold = 1190400;
 module_param(sync_threshold, uint, 0644);
 
-static unsigned int input_boost_freq;
+static unsigned int input_boost_freq = 1190400;
 module_param(input_boost_freq, uint, 0644);
 
 static unsigned int input_boost_ms = 40;
 module_param(input_boost_ms, uint, 0644);
+
+static unsigned int boost_ms = 20;
+module_param(boost_ms, uint, 0644);
 
 static u64 last_input_time;
 #define MIN_INPUT_INTERVAL (150 * USEC_PER_MSEC)
